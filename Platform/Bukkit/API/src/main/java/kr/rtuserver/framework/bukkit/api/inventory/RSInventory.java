718d14785a2d68cf033eca363260105a2e512742
IPECTER 이팩터 <80433772+IPECTER@users.noreply.github.com>
2025-03-18 22:21:08 +0900

    RSFramework 1.9.0 Rework RSInventory (add page inventory)

--- Platform/Bukkit/API/src/main/java/kr/rtuserver/framework/bukkit/api/inventory/RSInventory.java
+++ Platform/Bukkit/API/src/main/java/kr/rtuserver/framework/bukkit/api/inventory/RSInventory.java
@@ -3,51 +3,170 @@ package kr.rtuserver.framework.bukkit.api.inventory;
 import kr.rtuserver.framework.bukkit.api.RSPlugin;
 import kr.rtuserver.framework.bukkit.api.config.impl.SettingConfiguration;
 import kr.rtuserver.framework.bukkit.api.config.impl.TranslationConfiguration;
+import kr.rtuserver.framework.bukkit.api.utility.format.ComponentFormatter;
+import kr.rtuserver.framework.bukkit.api.utility.platform.MinecraftVersion;
+import kr.rtuserver.framework.bukkit.api.utility.player.PlayerChat;
 import lombok.Getter;
+import lombok.Setter;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-@Getter
-public abstract class RSInventory<T extends RSPlugin> implements InventoryHolder {
+public class RSInventory {
 
+    @Getter
+    public abstract static class Default<T extends RSPlugin> implements InventoryHolder {
 
-    private final T plugin;
-    private final SettingConfiguration setting;
-    private final TranslationConfiguration message;
-    private final TranslationConfiguration command;
+        private final T plugin;
+        private final SettingConfiguration setting;
+        private final TranslationConfiguration message;
+        private final TranslationConfiguration command;
+        private final PlayerChat chat;
 
-    public RSInventory(T plugin) {
-        this.plugin = plugin;
-        this.setting = plugin.getConfigurations().getSetting();
-        this.message = plugin.getConfigurations().getMessage();
-        this.command = plugin.getConfigurations().getCommand();
+        @Setter
+        private Inventory inventory;
+
+        public Default(T plugin) {
+            this.plugin = plugin;
+            this.setting = plugin.getConfigurations().getSetting();
+            this.message = plugin.getConfigurations().getMessage();
+            this.command = plugin.getConfigurations().getCommand();
+            this.chat = PlayerChat.of(plugin);
+        }
+
+        @NotNull
+        public Inventory getInventory() {
+            if (inventory == null) throw new UnsupportedOperationException("Not initialized inventory");
+            return inventory;
+        }
+
+        protected Inventory createInventory(InventoryType type, Component title) {
+            if (MinecraftVersion.isPaper()) return Bukkit.createInventory(this, type, title);
+            else return Bukkit.createInventory(this, type, ComponentFormatter.legacy(title));
+        }
+
+        protected Inventory createInventory(int size, Component component) {
+            if (MinecraftVersion.isPaper()) return Bukkit.createInventory(this, size, component);
+            else return Bukkit.createInventory(this, size, ComponentFormatter.legacy(component));
+        }
+
+        public boolean onClick(Event<InventoryClickEvent> event, Click click) {
+            return true;
+        }
+
+        public boolean onDrag(Event<InventoryDragEvent> event, Drag drag) {
+            return true;
+        }
+
+        public void onClose(Event<InventoryCloseEvent> event, Close close) {
+        }
+
+        public record Event<T extends InventoryEvent>(T event, Inventory inventory, Player player,
+                                                         boolean isInventory) {
+        }
+
+        public record Drag(Map<Integer, ItemStack> items, ItemStack cursor, ItemStack oldCursor, DragType type) {
+        }
+
+        public record Click(int slot, InventoryType.SlotType slotType, ClickType type) {
+        }
+
+        public record Close(InventoryCloseEvent.Reason reason) {
+        }
+
     }
 
-    public boolean onClick(Event<InventoryClickEvent> event, Click click) {
-        return true;
+    @Getter
+    public abstract static class Page<T extends RSPlugin> implements InventoryHolder {
+
+        private final T plugin;
+        private final SettingConfiguration setting;
+        private final TranslationConfiguration message;
+        private final TranslationConfiguration command;
+        private final PlayerChat chat;
+
+        private final List<Inventory> pages = new ArrayList<>();
+        @Setter
+        private int page = 0;
+
+        public Page(T plugin) {
+            this.plugin = plugin;
+            this.setting = plugin.getConfigurations().getSetting();
+            this.message = plugin.getConfigurations().getMessage();
+            this.command = plugin.getConfigurations().getCommand();
+            this.chat = PlayerChat.of(plugin);
+        }
+
+        @NotNull
+        public Inventory getInventory() {
+            Inventory inventory = pages.get(page);
+            if (inventory == null) throw new UnsupportedOperationException("Not initialized inventory");
+            return inventory;
+        }
+
+        protected Inventory createInventory(InventoryType type, Component title) {
+            if (MinecraftVersion.isPaper()) return Bukkit.createInventory(this, type, title);
+            else return Bukkit.createInventory(this, type, ComponentFormatter.legacy(title));
+        }
+
+        protected Inventory createInventory(int size, Component component) {
+            if (MinecraftVersion.isPaper()) return Bukkit.createInventory(this, size, component);
+            else return Bukkit.createInventory(this, size, ComponentFormatter.legacy(component));
+        }
+
+        protected abstract void load(int page);
+
+        protected boolean previous() {
+            if (page <= 0) return false;
+            load(--page);
+            return true;
+        }
+
+        protected boolean next() {
+            if (page >= pages.size() - 1) return false;
+            load(++page);
+            return true;
+        }
+
+        protected Inventory addPage(int page, int size, Component component) {
+            Inventory inventory = createInventory(size, component);
+            pages.add(page, inventory);
+            return inventory;
+        }
+
+        public boolean onClick(Event<InventoryClickEvent> event, Click click) {
+            return true;
+        }
+
+        public boolean onDrag(Event<InventoryDragEvent> event, Drag drag) {
+            return true;
+        }
+
+        public void onClose(Event<InventoryCloseEvent> event, Close close) {
+        }
+
+        public record Event<T extends InventoryEvent>(T event, Inventory inventory, Player player,
+                                                         boolean isInventory, int page) {
+        }
+
+        public record Drag(Map<Integer, ItemStack> items, ItemStack cursor, ItemStack oldCursor, DragType type) {
+        }
+
+        public record Click(int slot, InventoryType.SlotType slotType, ClickType type) {
+        }
+
+        public record Close(InventoryCloseEvent.Reason reason) {
+        }
+
     }
 
-    public boolean onDrag(Event<InventoryDragEvent> event, Drag drag) {
-        return true;
-    }
-
-    public void onClose(Event<InventoryCloseEvent> event, Close close) {
-    }
-
-    public record Event<T extends InventoryEvent>(T event, Inventory inventory, Player player, boolean isInventory) {
-    }
-
-    public record Drag(Map<Integer, ItemStack> items, ItemStack cursor, ItemStack oldCursor, DragType type) {
-    }
-
-    public record Click(int slot, InventoryType.SlotType slotType, ClickType type) {
-    }
-
-    public record Close(InventoryCloseEvent.Reason reason) {
-    }
 }
--- Platform/Bukkit/Core/src/main/java/kr/rtuserver/framework/bukkit/core/internal/listeners/InventoryListener.java
+++ Platform/Bukkit/Core/src/main/java/kr/rtuserver/framework/bukkit/core/internal/listeners/InventoryListener.java
@@ -11,6 +11,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 
 public class InventoryListener extends RSListener<RSPlugin> {
 
@@ -22,10 +23,12 @@ public class InventoryListener extends RSListener<RSPlugin> {
     private void onRSInventoryClick(InventoryClickEvent e) {
         Inventory inv = e.getClickedInventory();
         Player player = (Player) e.getWhoClicked();
-        if (e.getView().getTopInventory().getHolder() instanceof RSInventory<? extends RSPlugin> rsInv) {
-            boolean isPlayerInventory = inv != null && !(inv.getHolder() instanceof RSInventory);
-            RSInventory.Event<InventoryClickEvent> event = new RSInventory.Event<>(e, inv, player, isPlayerInventory);
-            RSInventory.Click click = new RSInventory.Click(e.getSlot(), e.getSlotType(), e.getClick());
+        InventoryHolder holder = e.getView().getTopInventory().getHolder();
+
+        if (holder instanceof RSInventory.Default<? extends RSPlugin> rsInv) {
+            boolean isPlayerInventory = inv != null && !(inv.getHolder() instanceof RSInventory.Default);
+            RSInventory.Default.Event<InventoryClickEvent> event = new RSInventory.Default.Event<>(e, inv, player, isPlayerInventory);
+            RSInventory.Default.Click click = new RSInventory.Default.Click(e.getSlot(), e.getSlotType(), e.getClick());
             try {
                 e.setCancelled(!rsInv.onClick(event, click));
             } catch (Exception ex) {
@@ -35,6 +38,23 @@ public class InventoryListener extends RSListener<RSPlugin> {
                 getPlugin().getAdventure().player(player).sendMessage(errorMessage);
                 ex.printStackTrace();
             }
+            return;
+        }
+
+        if (holder instanceof RSInventory.Page<? extends RSPlugin> rsInv) {
+            boolean isPlayerInventory = inv != null && !(inv.getHolder() instanceof RSInventory.Page);
+            RSInventory.Page.Event<InventoryClickEvent> event = new RSInventory.Page.Event<>(e, inv, player, isPlayerInventory, rsInv.getPage());
+            RSInventory.Page.Click click = new RSInventory.Page.Click(e.getSlot(), e.getSlotType(), e.getClick());
+            try {
+                e.setCancelled(!rsInv.onClick(event, click));
+            } catch (Exception ex) {
+                e.setCancelled(true);
+                Component errorMessage = ComponentFormatter.mini(getMessage().get(player, "error.inventory"));
+                getPlugin().console(errorMessage);
+                getPlugin().getAdventure().player(player).sendMessage(errorMessage);
+                ex.printStackTrace();
+            }
+            return;
         }
     }
 
@@ -42,10 +62,12 @@ public class InventoryListener extends RSListener<RSPlugin> {
     private void onRSInventoryDrag(InventoryDragEvent e) {
         Inventory inv = e.getInventory();
         Player player = (Player) e.getWhoClicked();
-        if (e.getView().getTopInventory().getHolder() instanceof RSInventory<? extends RSPlugin> rsInv) {
-            boolean isPlayerInventory = inv != null && !(inv.getHolder() instanceof RSInventory);
-            RSInventory.Event<InventoryDragEvent> holderEvent = new RSInventory.Event<>(e, inv, player, isPlayerInventory);
-            RSInventory.Drag drag = new RSInventory.Drag(e.getNewItems(), e.getCursor(), e.getOldCursor(), e.getType());
+        InventoryHolder holder = e.getView().getTopInventory().getHolder();
+
+        if (holder instanceof RSInventory.Default<? extends RSPlugin> rsInv) {
+            boolean isPlayerInventory = !(inv.getHolder() instanceof RSInventory.Default);
+            RSInventory.Default.Event<InventoryDragEvent> holderEvent = new RSInventory.Default.Event<>(e, inv, player, isPlayerInventory);
+            RSInventory.Default.Drag drag = new RSInventory.Default.Drag(e.getNewItems(), e.getCursor(), e.getOldCursor(), e.getType());
             try {
                 e.setCancelled(!rsInv.onDrag(holderEvent, drag));
             } catch (Exception ex) {
@@ -55,6 +77,23 @@ public class InventoryListener extends RSListener<RSPlugin> {
                 getPlugin().getAdventure().player(player).sendMessage(errorMessage);
                 ex.printStackTrace();
             }
+            return;
+        }
+
+        if (holder instanceof RSInventory.Page<? extends RSPlugin> rsInv) {
+            boolean isPlayerInventory = !(inv.getHolder() instanceof RSInventory.Page);
+            RSInventory.Page.Event<InventoryDragEvent> holderEvent = new RSInventory.Page.Event<>(e, inv, player, isPlayerInventory, rsInv.getPage());
+            RSInventory.Page.Drag drag = new RSInventory.Page.Drag(e.getNewItems(), e.getCursor(), e.getOldCursor(), e.getType());
+            try {
+                e.setCancelled(!rsInv.onDrag(holderEvent, drag));
+            } catch (Exception ex) {
+                e.setCancelled(true);
+                Component errorMessage = ComponentFormatter.mini(getMessage().get(player, "error.inventory"));
+                getPlugin().console(errorMessage);
+                getPlugin().getAdventure().player(player).sendMessage(errorMessage);
+                ex.printStackTrace();
+            }
+            return;
         }
     }
 
@@ -62,10 +101,12 @@ public class InventoryListener extends RSListener<RSPlugin> {
     private void onRSInventoryClose(InventoryCloseEvent e) {
         Inventory inv = e.getInventory();
         Player player = (Player) e.getPlayer();
-        if (inv.getHolder() instanceof RSInventory<? extends RSPlugin> rsInv) {
-            boolean isPlayerInventory = !(inv.getHolder() instanceof RSInventory);
-            RSInventory.Event<InventoryCloseEvent> holderEvent = new RSInventory.Event<>(e, inv, player, isPlayerInventory);
-            RSInventory.Close close = new RSInventory.Close(e.getReason());
+        InventoryHolder holder = e.getView().getTopInventory().getHolder();
+
+        if (holder instanceof RSInventory.Default<? extends RSPlugin> rsInv) {
+            boolean isPlayerInventory = !(inv.getHolder() instanceof RSInventory.Default);
+            RSInventory.Default.Event<InventoryCloseEvent> holderEvent = new RSInventory.Default.Event<>(e, inv, player, isPlayerInventory);
+            RSInventory.Default.Close close = new RSInventory.Default.Close(e.getReason());
             try {
                 rsInv.onClose(holderEvent, close);
             } catch (Exception ex) {
@@ -74,6 +115,22 @@ public class InventoryListener extends RSListener<RSPlugin> {
                 getPlugin().getAdventure().player(player).sendMessage(errorMessage);
                 ex.printStackTrace();
             }
+            return;
+        }
+
+        if (holder instanceof RSInventory.Page<? extends RSPlugin> rsInv) {
+            boolean isPlayerInventory = !(inv.getHolder() instanceof RSInventory.Page);
+            RSInventory.Page.Event<InventoryCloseEvent> holderEvent = new RSInventory.Page.Event<>(e, inv, player, isPlayerInventory, rsInv.getPage());
+            RSInventory.Page.Close close = new RSInventory.Page.Close(e.getReason());
+            try {
+                rsInv.onClose(holderEvent, close);
+            } catch (Exception ex) {
+                Component errorMessage = ComponentFormatter.mini(getMessage().get(player, "error.inventory"));
+                getPlugin().console(errorMessage);
+                getPlugin().getAdventure().player(player).sendMessage(errorMessage);
+                ex.printStackTrace();
+            }
+            return;
         }
     }
 
