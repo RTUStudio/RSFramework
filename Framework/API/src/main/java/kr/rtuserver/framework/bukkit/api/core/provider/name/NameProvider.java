1780da8729635dcfcfe2718144cfad17ec5d4dcf
IPECTER 이팩터 <80433772+IPECTER@users.noreply.github.com>
2025-06-29 07:34:33 +0900

    RSFramework 2.7.1-beta.5 Refactor: NameProvider

--- Framework/API/src/main/java/kr/rtuserver/framework/bukkit/api/core/provider/name/NameProvider.java
+++ Framework/API/src/main/java/kr/rtuserver/framework/bukkit/api/core/provider/name/NameProvider.java
@@ -4,13 +4,18 @@ import kr.rtuserver.framework.bukkit.api.core.provider.Provider;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface NameProvider extends Provider {
 
     List<String> getNames();
 
+    List<String> getNames(boolean includeProxy);
+
     String getName(Player player);
 
     Player getPlayer(String name);
+
+    UUID getUniqueId(String name);
 
 }
--- Framework/Core/src/main/java/kr/rtuserver/framework/bukkit/core/provider/name/VanillaNameProvider.java
+++ Framework/Core/src/main/java/kr/rtuserver/framework/bukkit/core/provider/name/VanillaNameProvider.java
@@ -1,17 +1,28 @@
 package kr.rtuserver.framework.bukkit.core.provider.name;
 
 import kr.rtuserver.framework.bukkit.api.core.provider.name.NameProvider;
+import kr.rtuserver.framework.bukkit.api.player.RSPlayer;
+import kr.rtuserver.framework.bukkit.core.Framework;
+import kr.rtuserver.protoweaver.api.proxy.ProxyPlayer;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.List;
+import java.util.UUID;
 
 public class VanillaNameProvider implements NameProvider {
 
     @Override
     public List<String> getNames() {
         return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
+    }
+
+    @Override
+    public List<String> getNames(boolean includeProxy) {
+        if (includeProxy) return RSPlayer.getPlayers(true).stream().map(ProxyPlayer::getName).toList();
+        else return getNames();
     }
 
     @Override
@@ -22,6 +33,17 @@ public class VanillaNameProvider implements NameProvider {
     @Override
     public Player getPlayer(String name) {
         return Bukkit.getPlayer(name);
+    }
+
+    /**
+     * 프록시 플레이어를 지원합니다
+     */
+    @Override
+    public UUID getUniqueId(String name) {
+        for (ProxyPlayer player : RSPlayer.getPlayers()) {
+            if (player.getName().equals(name)) return player.getUniqueId();
+        }
+        return null;
     }
 
     @Override
